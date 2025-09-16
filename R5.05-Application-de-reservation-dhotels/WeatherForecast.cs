568feaef9da297907cdd6d757c4a4ef68e185831
Enzo Rynders-Vitu <enzo.ryndersvitu@gmail.com>
2025-09-16 10:18:48 +0200

    Ajoutez des fichiers projet.

---
+++ R5.05-Application-de-reservation-dhotels/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace R5._05_Application_de_reservation_dhotels
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
